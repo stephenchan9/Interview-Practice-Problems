b2da9d5c8e722232bf9d9db4a2efa2df67f7549a
Stephen <stephen.chan9@yahoo.com>
2022-01-05 13:33:29 -0800

    Update 3sum solution

--- Javascript/3sum.js
+++ Javascript/3sum.js
@@ -1,48 +1,54 @@
-// 3sum
-// The idea is to first sort the array from small to large values.
-// We will use two pointers to move in our array to find the 3 sum combination while trying to minmize run time. we also need to make sure there are no duplicates.
-// Since we 
-let threesum = function (arr) {
-  let result = [];
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+    let result = [];
+    
+    // sort the array so that's it increasing. This adds o(nlogn) complexity.
+    nums = nums.sort((a,b) => a-b)
+    
+    // loop through the sorted array.
+    // Since we're using 2 poiunters there's no need for index i to check the last 2 numbers in the array.
+    for(let i = 0; i < nums.length - 2; i++){
+        // We add a if condition here to check for duplicates.
+        // If index i is at 0 then we work with this value no matter what
+        // If index is greater than 0, we then check if the previous number is the same as current number. Skip it if its duplicate
+        if(i === 0 || (i > 0 && nums[i] !== nums[i-1])){
+            // Now we know its not a duplicate
+            let low = i+1;
+            let high = nums.length - 1;
+                        
+            // We loop to handle moving our low and high pointers.
+            // If our low pointer moves past the high pointer, then we stop the loop and move our index i.
+            while (low < high){
+                let sum = nums[i] + nums[low] + nums[high];
 
-  // sort the array first
-  arr = arr.sort((a, b) => a - b);
-
-  // Loop through the array. We do - 2 because we use 2 pointers so there's no need to check the whole array.
-  for (let i = 0; i < arr.length - 2; i++) {
-    // we check i here to verify its not a duplicate value. 
-    // 1. If the index is at 0 then we check this number.
-    // 2. If the index is not 0, we check if the number at the index is the same as the number before it. If it is then we skip it since its a duplicate.
-    if (i === 0 || (i > 0 && arr[i] !== arr[i - 1])) {
-      // Now we don't have to hit duplicates.
-      let low = i + 1;
-      let high = arr.length - 1;
-
-      let sum = arr[i] + arr[low] + arr[high];
-
-      while (low < high) {
-        if (sum === 0) {
-          result.push([arr[i], arr[low], arr[high]]);
-          // we want to avoid duplicates so move our pointers until they're different numbers
-          while (low < high && arr[low] == arr[low + 1]) {
-            low++;
-          }
-          while (low < high && arr[high] == arr[high - 1]) {
-            high--;
-          }
-          low++;
-          high--;
-        } else if (sum < 0) {
-          low++;
-        } else {
-          high--;
+                if (sum === 0){
+                    // Then we found  triplet
+                    result.push([nums[i], nums[low], nums[high]]);
+                    
+                    // Check for duplicates. We keep moving our pointers until they aren't next to a duplicate.
+                    while (nums[low] === nums[low+1]){
+                        low++;
+                    }
+                    while (nums[high] === nums[high - 1]){
+                        high--
+                    }
+                    
+                    // Move our pointers.
+                    low++;
+                    high--;
+                } else if (sum < 0){
+                    // move our low pointer 
+                    low++;
+                } else {
+                    // move our high pointer
+                    high--;
+                }
+            }
         }
-      }
     }
-  }
-  console.log(result);
-  return result;
+    
+    return result;
 };
-
-
-threesum([-1,0,1,2,-1,-4]);
